74dd9bd08d463d354f0512a1b9db140a26fd92fa
Pavel Holba <pavel.holba@math.slu.cz>
2024-06-10 17:08:08 +0200

    tady?

--- process_monitoring.sh
+++ process_monitoring.sh
@@ -5,14 +5,15 @@ current_processes=$(ps aux)
 check_new_processes() {
     new_processes=$(ps aux)
     while read -r line; do
-        # Extract the user name, cpu, mem and process name from the process line
+        # Extract the user name, cpu and mem from the process line
         user=$(echo "$line" | awk '{print $1}')
         cpu=$(echo "$line" | awk '{print $3}')
         mem=$(echo "$line" | awk '{print $4}')
-        process=$(echo "$line" | awk '{print $11}')
+        # Extract the process name which is the rest of the line
+        process=$(echo "$line" | awk '{print substr($0,index($0,$11))}') 
 
         # Ignore processes owned by root, systemd+, message+, polkitd, and syslog
-        if [[ "$process" != "ps" && "$user" != "root" && "$user" != "systemd+" && "$user" != "message+" && "$user" != "polkitd" && "$user" != "syslog" ]]; then
+        if [[ "$process" != "ps aux" && "$user" != "root" && "$user" != "systemd+" && "$user" != "message+" && "$user" != "polkitd" && "$user" != "syslog" ]]; then
             if ! echo "$current_processes" | grep -q "$line"; then
                 echo "New Process Detected: $line"
             fi
