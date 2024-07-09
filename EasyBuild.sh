28af51c0c081f0d833443f9781794d98be6cd93d
Pavel Holba <pavel.holba@vsb.cz>
2024-07-09 08:01:55 +0200

    Initial EasyBuild instalation script

---
+++ EasyBuild.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo
+echo "## Installing EasyBuild ##"
+echo
+
+# install pip first
+sudo apt install python3-pip
+
+# install EasyBuild to temporary location
+export EB_TMPDIR=/tmp/$USER/eb_tmp
+python3.10 -m pip install --ignore-installed --prefix $EB_TMPDIR easybuild
+
+# set PATHS and instal easybuild using easybuild
+export EB_TMPDIR=/tmp/$USER/eb_tmp
+export PATH=$EB_TMPDIR/local/bin:$PATH
+export PYTHONPATH=$EB_TMPDIR/local/lib/python3.6/dist-packages:$PYTHONPATH
+export EB_PYTHON=python3.6
+eb --install-latest-eb-release --prefix $HOME/easybuild
+
+echo
+echo "## EasyBuild should be installed ##"
+echo
