77ddc69b8f320809979328857bc6dc8b7212b018
Pavel Holba <pavel.holba@vsb.cz>
2024-07-10 08:46:04 +0200

    typo

--- basics.sh
+++ basics.sh
@@ -11,6 +11,11 @@ echo
 echo "## Provisioning New Ubuntu Linux Server (Basic) ##"
 echo
 
+# we first need to repair /etc/apt/sources.list, uncomment all deb-src lines
+sudo sed -i -e 's/^# deb/deb/g' /etc/apt/sources.list
+# some 24.04 Ubuntu have different location and format, so we may need to run this
+sudo sed -i 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources
+
 # Update package lists
 sudo apt-get update
 
