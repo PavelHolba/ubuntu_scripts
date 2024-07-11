c033ead5621fc054509b70f07bdbe00a5210278f
Pavel Holba <pavel.holba@vsb.cz>
2024-07-11 15:17:36 +0200

    python 3.10

--- EasyBuild.sh
+++ EasyBuild.sh
@@ -8,18 +8,20 @@ echo "## Installing EasyBuild ##"
 echo
 
 # install pip first
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 
 # install EasyBuild to temporary location
 export EB_TMPDIR=/tmp/$USER/eb_tmp
 python3.10 -m pip install --ignore-installed --prefix $EB_TMPDIR easybuild
 
 # set PATHS and instal easybuild using easybuild
-export EB_TMPDIR=/tmp/$USER/eb_tmp
 export PATH=$EB_TMPDIR/local/bin:$PATH
-export PYTHONPATH=$EB_TMPDIR/lib*/python3.10/site-packages:$PYTHONPATH
+export PYTHONPATH=$EB_TMPDIR/local/lib/python3.10/dist-packages:$PYTHONPATH
 export EB_PYTHON=python3.10
-eb --install-latest-eb-release --prefix $HOME/easybuild
+
+# lastly we need to say Lmod, where he can find it
+module use /home/hol0598/easybuild/modules/all/
+# module load EasyBuild
 
 echo
 echo "## EasyBuild should be installed ##"
